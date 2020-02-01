08c7298e4dfe2afcbdd0e93e2b21a37ad9769b82
Mike Conrad <mike@enxo.co>
2020-02-01 17:47:37 -0500

    Initial commit, split off from openSuites

---
+++ popup.js
@@ -0,0 +1,65 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+snapToast.style.display = 'none'
+  let secret = document.getElementById('secret')
+
+  let serverUrl = 'https://nxone.co'
+  const snapsUrl = `${serverUrl}/api/snaps/`;
+
+  snaps.onsubmit = async (e) => {
+    e.preventDefault();
+
+    fetch(`${serverUrl}/api/snaps/`, {
+      method: 'POST',
+      body: new FormData(snaps)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        console.log('Network not available')
+        // throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((response) => {
+      let result = response
+      snapToast.style.display = 'flex'
+      snapText.value = serverUrl + '/l/' + result.url
+    })
+    .catch((error) => {
+      snapToast.style.display = 'flex'
+      snapText.value = 'Sorry the server is currently experiencing issues.  Please try again later'
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+
+  };
+
+  shorts.onsubmit = async (e) => {
+    e.preventDefault();
+
+    fetch(`${serverUrl}/api/shorts/`, {
+      method: 'POST',
+      body: new FormData(shorts)
+    })
+    .then((response) => {
+      if (!response.ok) {
+        console.log('Network not available')
+        // throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((response) => {
+      let result = response
+      shortToast.style.display = 'flex'
+      shortText.value = serverUrl + '/l/' + result.url
+    })
+    .catch((error) => {
+      shortToast.style.display = 'flex'
+      shortText.value = 'Sorry the server is currently experiencing issues.  Please try again later'
+      console.error('There has been a problem with your fetch operation:', error);
+    });
+
+  };
